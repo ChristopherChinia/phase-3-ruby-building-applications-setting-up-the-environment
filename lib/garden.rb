24b80e611b73155357a9297669cf4d23a46a6240
Christopherchinia <chinia214@gmail.com>
2022-08-21 06:17:29 +0300

    Done.

--- lib/garden.rb
+++ lib/garden.rb
@@ -11,3 +11,12 @@ class Garden
     end
   end
 end
+lawn = Garden.new(name: 'Front Lawn')
+
+basil = Plant.new(name: 'Basil')
+basil.garden = lawn
+
+cucumber = Plant.new(name: 'Cucumber')
+cucumber.garden = lawn
+
+p lawn.plants
